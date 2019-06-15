81526c3339bcce29d09547c588af3de3ec0f5369
DESKTOP-TB88SQV\ezackx <qkrtmdwls280@gmail.com>
2019-06-15 11:53:07 +0900

    푸시~

---
+++ src/main/java/itc/hoseo/cppe/controller/MypageController.java
@@ -0,0 +1,48 @@
+package itc.hoseo.cppe.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import itc.hoseo.cppe.member.Member;
+import itc.hoseo.cppe.member.MemberService;
+
+@Controller
+public class MypageController {
+
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping(value = "/mypage", method = { RequestMethod.GET, RequestMethod.POST })
+	public String mypage(ModelMap model, HttpSession session) throws Exception{
+		Member m =  (Member) session.getAttribute("member");
+		String id = m.getId();
+			model.put("user",memberService.getUser(id));
+			return "mypage/mypage";
+	}
+	
+	@RequestMapping(value = "/mypageEdit", method = { RequestMethod.GET, RequestMethod.POST })
+	public String mypageEdit(ModelMap model, HttpSession session) throws Exception{
+		Member m =  (Member) session.getAttribute("member");
+		String id = m.getId();
+			model.put("user",memberService.getUser(id));
+			return "mypage/mypageEdit";
+	}
+	
+	@RequestMapping(value = "/mypageEdit/update", method = { RequestMethod.GET, RequestMethod.POST })
+	public String mypageEdit1(ModelMap model, HttpSession session, Member member) {
+		Member m =  (Member) session.getAttribute("member");
+		String id = (String) session.getAttribute("id");
+		if (memberService.updateUser(id,member)==1) {
+			return "redirect:mypage/mypage";
+		} else {
+			return "mypage/mypageEdit";
+		}
+	}
+}
