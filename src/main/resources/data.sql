c71331d83102be63fc24a7a0d35c9ecc99b70877
qkrtm <qkrtm@DESKTOP-77PS9GQ>
2019-09-21 10:54:33 +0900

    로그인완료(로그인실패시 메시지띄우기)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 Insert into member values('tmdwls2805', 'tmdwls12', '박승진', 'qkrtmdwls280@gmail.com','01040702805','123','경기','안산','0',now());
+Insert into member values('admin', 'admin', '박승진', 'admin@gmail.com','01040702805','123','경기','안산','0',now());
