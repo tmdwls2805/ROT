116dbe9d8bf21d955e56a2ceef4690289125d7ad
qkrtm <qkrtm@DESKTOP-77PS9GQ>
2019-10-23 19:06:01 +0900

    [mypage designing..]

--- src/main/java/com/hoseo/rot/controller/MypageController.java
+++ src/main/java/com/hoseo/rot/controller/MypageController.java
@@ -6,19 +6,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MypageController {
 	
+	
+	//레시피 노트
 	@GetMapping("/recipeNote")
 	public String recipeNote() {
 		return "mypage/recipeNote";
 	}
-
-	@GetMapping("/MyHome")
-	public String MyHome() {
-		return "mypage/MyHome";
+	
+	// 프로필 사진 수정
+	@GetMapping("/updateProfileImg")
+	public String updateProfileImg() {
+		return "mypage/updateProfileImg";
 	}
 	
-	@GetMapping("/sidebar")
-	public String sidebar() {
-		return "mypage/sidebar";
+	// 내가 쓴 게시물
+	@GetMapping("/writeBoard")
+	public String writeBoard() {
+		return "mypage/writeBoard";
 	}
+	
+	// 주문조회
+	@GetMapping("/orderInquiry")
+	public String orderInquiry() {
+		return "mypage/orderInquiry";
+	}
+	
+	// 장바구니
+	@GetMapping("/cart")
+	public String cart() {
+		return "mypage/cart";
+	}
+	
+	//회원정보수정
+	@GetMapping("/updateMemberInfo")
+	public String MyHome() {
+		return "mypage/updateMemberInfo";
+	}
+	
+	//문의내역
+	@GetMapping("/contactHistory")
+	public String contactHistory() {
+		return "mypage/contactHistory";
+	}	
+	
 
 }
