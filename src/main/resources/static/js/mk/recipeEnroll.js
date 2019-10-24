c6b8a8dea19cddbcd94a2fd3d543d7f8067f52cf
qkrtm <qkrtm@192.168.0.23>
2019-10-24 20:07:47 +0900

    메뉴 삭제 완료

--- src/main/resources/static/js/mk/recipeEnroll.js
+++ src/main/resources/static/js/mk/recipeEnroll.js
@@ -4,12 +4,12 @@ $("#add").on("click", function(){
 			<div class="input-mat2">
 				<input type="text" class="input-mat2Find" name="mat2Find" placeholder="예) 미역" /> 
 				<input type="text" class="input-mat2Vol" name="mat2Vol" placeholder="예) 100g" />
-				<button class="deleteButton" type="button" onclick="deleteBox(this)"><img class="deleteImg" th:src=@{/img/mk/delete.png}></button>
+				<button class="deleteButton" type="button" onclick="deleteBox(this)"><img class="deleteImg" src="/img/mk/delete.png"></button>
 			</div>										
 		</li>`);
 });
 function deleteBox(self){
-	$($(self).parent('li')[0]).remove();	
+	$($(self).parent('div').parent('li')).remove();	
 }
 
 
